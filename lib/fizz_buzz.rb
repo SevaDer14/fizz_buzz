b82b71a5971c37f32ca8ae01a9acd28aa44dde61
sevader14 <vsevolod.deriushkin@gmail.com>
2021-03-10 16:15:25 +0100

    Refactores and Comments the Code

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,19 +1,26 @@
+# Checks number for divisablility on a divider
+def has_zero_remainder?(number, divider)
+    number % divider == 0
+end
+
+# FizzBuzz Method
 def fizz_buzz(number)
+    # Checks if user input is string 
     check = number.is_a? String
     if check == true
         'Error#1: Please enter a positive number'
+    # Algo for FizzBuzz
     else        
-        if number == 0
+        if number == 0 # Special case if user inputs zero
             'Error#2: You hacked the universe! The answer is Quantum superposition of fizz, buzz, fizzbuzz and 0..'        
-        elsif number % 15 == 0
+        elsif has_zero_remainder(number, 15) # 15 first as it is itself divisable by 3 and 5
             'fizzbuzz'
-        elsif number % 3 == 0
+        elsif has_zero_remainder(number, 3)
             'fizz'
-        elsif number % 5 == 0
+        elsif has_zero_remainder(number, 3)
             'buzz'    
         else
             number
         end
-    end
-    
+    end    
 end
