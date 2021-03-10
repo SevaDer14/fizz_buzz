2d268da1d01aba65979eb3fef4736b657fe144cf
sevader14 <vsevolod.deriushkin@gmail.com>
2021-03-10 15:24:39 +0100

    working FizzBuzz with passed tests

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,12 @@
 def fizz_buzz(number)
-    number
-  end
+    
+    if number % 15 == 0
+        'fizzbuzz'
+    elsif number % 3 == 0
+        'fizz'
+    elsif number % 5 == 0
+        'buzz'    
+    else
+        number
+    end
+end
